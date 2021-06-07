cf08c8ab78f83683618070dda213ce78bd1fa188
Mark <mark74384@gmail.com>
2021-06-07 22:02:21 +0200

    mmm

--- MainProjects/JavaScript Project/helloWorld.js
+++ MainProjects/JavaScript Project/helloWorld.js
@@ -1,1 +1,30 @@
-console.log("hello World")
+var TreeFruit = function(weight , fruit){
+    var node = Object.create(TreeFruit.prototype)
+        node.weight = weight;
+        node.fruit = fruit;
+        node.right = undefined;
+        node.left = undefined;
+    return node 
+};
+
+TreeFruit.prototype.insert = function(weight , fruit){
+    var temp = TreeFruit(weight , fruit);
+    function recurse(bst) {
+         if (bst.weight > weight && bst.left === undefined) {
+          bst.left = temp;
+        } else if (bst.weight > weight) {
+          recurse(bst.left);
+        } else if (bst.weight < weight && bst.right === undefined) {
+          bst.right = temp;
+        } else if (bst.weight < weight) {
+          recurse(bst.right);
+        }
+      }
+      recurse(this);
+}
+
+var a = Object.create(TreeFruit);
+a.prototype.insert(15,2);
+console.log(a.prototype.weight);
+
+
