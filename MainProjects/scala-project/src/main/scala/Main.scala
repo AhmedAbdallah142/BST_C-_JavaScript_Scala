6cb42199873bf546858256d7240e3b481be12fbf
ahmed <ahmed@192.168.1.6>
2021-06-07 16:08:25 +0200

    tests - add informations

--- MainProjects/scala-project/src/main/scala/Main.scala
+++ MainProjects/scala-project/src/main/scala/Main.scala
@@ -11,27 +11,36 @@ class Berry extends Tiny_Fruit{
 
 }
 class Grapes extends Tiny_Fruit{
-
+ this.weight = 140; //21 grapes
 }
 class Apple extends Oval_Shaped{
-
+  this.weight = 150;
 }
 class Avocado extends Oval_Shaped{
-  
+   this.weight = 130;
 }
 class Blackberries extends Berry{
   
 }
 class Elderberries extends Berry{
-  
+   this.weight = 77;
 }
 class Gooseberries extends Berry{
   
 }
 class Blueberries extends Berry{
-  
+   this.weight = 80;
 }
 
 object Main extends App {
-  println("Ahmed!")
+  var a : Fruit = new Apple();
+  println(a.weight);
+  a = new Blueberries();
+  println(a.weight);
+  a = new Berry();
+  println(a.weight);
+  a = new Avocado();
+  println(a.weight);
+  a = new Grapes();
+  println(a.weight);
 }
