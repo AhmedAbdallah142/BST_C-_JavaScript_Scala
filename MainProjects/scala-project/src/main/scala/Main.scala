765b668c64f02143e5019615725932bfd1cc5392
ahmed <ahmed@192.168.1.6>
2021-06-07 15:55:31 +0200

    fruit classes added

--- MainProjects/scala-project/src/main/scala/Main.scala
+++ MainProjects/scala-project/src/main/scala/Main.scala
@@ -1,6 +1,37 @@
-object Main extends App {
-  println("Hello, World!")
-  println("Hello, World!")
-  println("Hello, World!")
+class Fruit{
+  var weight :Int = 0;
+} 
+class Tiny_Fruit extends Fruit{
 
+} 
+class Oval_Shaped extends Fruit{
+
+}
+class Berry extends Tiny_Fruit{
+
+}
+class Grapes extends Tiny_Fruit{
+
+}
+class Apple extends Oval_Shaped{
+
+}
+class Avocado extends Oval_Shaped{
+  
+}
+class Blackberries extends Berry{
+  
+}
+class Elderberries extends Berry{
+  
+}
+class Gooseberries extends Berry{
+  
+}
+class Blueberries extends Berry{
+  
+}
+
+object Main extends App {
+  println("Ahmed!")
 }
