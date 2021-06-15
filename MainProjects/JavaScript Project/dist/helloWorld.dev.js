cc995a7d8dd49bc2e9fad07d4f9012c5dd2b8da4
Mark <mark74384@gmail.com>
2021-06-15 20:38:29 +0200

    finish

--- MainProjects/JavaScript Project/dist/helloWorld.dev.js
+++ MainProjects/JavaScript Project/dist/helloWorld.dev.js
@@ -1,39 +1,35 @@
+"use strict";
+
 var Fruit = {
   weight: 0,
   type: "fruit",
-  setWeight: function (weight) {
+  setWeight: function setWeight(weight) {
     this.weight = weight;
   },
-  getWeight: function () {
+  getWeight: function getWeight() {
     return this.weight;
   },
-  setType: function (type) {
+  setType: function setType(type) {
     this.type = type;
   },
-  getType: function () {
+  getType: function getType() {
     return this.type;
-  },
+  }
 };
-
-
-
 var apple = Object.create(Fruit);
 apple.setWeight(180);
 apple.setType("apple fruit");
-
 var orange = Object.create(Fruit);
 orange.setWeight(150);
 orange.setType("orange fruit");
-
 var mango = Object.create(Fruit);
 mango.setWeight(200);
 mango.setType("mango fruit");
-
 var tomato = Object.create(Fruit);
 tomato.setWeight(100);
 tomato.setType("tomato fruit");
 
-var FruitBST = function (value) {
+var FruitBST = function FruitBST(value) {
   var root = Object.create(FruitBST.prototype);
   root.value = value;
   root.left = undefined;
@@ -55,6 +51,7 @@ FruitBST.prototype.insert = function (value) {
       findplace(bst.right);
     }
   }
+
   findplace(this);
 };
 
@@ -63,29 +60,35 @@ FruitBST.prototype.inorder = function (myfunction) {
     if (bst === undefined) {
       return;
     }
+
     iter(bst.left);
     myfunction(bst);
     iter(bst.right);
   }
+
   iter(this);
 };
 
 FruitBST.prototype.iterate = function () {
   this.inorder(iterate2);
+
   function iterate2(bst) {
     if (bst === undefined) {
       return;
     }
+
     console.log(bst.value);
   }
 };
 
 FruitBST.prototype.filterbytype = function (type) {
   this.inorder(filttype);
+
   function filttype(bst) {
     if (bst === undefined) {
       return;
     }
+
     if (bst.value.type === type) {
       console.log(bst.value);
     }
@@ -94,10 +97,12 @@ FruitBST.prototype.filterbytype = function (type) {
 
 FruitBST.prototype.filterByWeight = function (weight) {
   this.inorder(filtweight);
+
   function filtweight(bst) {
     if (bst === undefined) {
       return;
     }
+
     if (bst.value.weight === weight) {
       console.log(bst.value);
     }
@@ -105,53 +110,63 @@ FruitBST.prototype.filterByWeight = function (weight) {
 };
 
 FruitBST.prototype.magnifyByType = function (type, weight) {
-  const list = [];
+  var list = [];
   this.inorder(magnifyByType2);
+
   function magnifyByType2(bst) {
     if (bst === undefined) {
       return;
     }
+
     if (bst.value.type === type) {
       var newWeight = bst.value.getWeight() + weight;
       bst.value.setWeight(newWeight);
     }
+
     list.push(bst.value);
   }
+
   var newbst = FruitBST(list[0]);
-  for(i=1 ; i<list.length ; i++){
-      newbst.insert(list[i]);
+
+  for (i = 1; i < list.length; i++) {
+    newbst.insert(list[i]);
   }
+
   return newbst;
 };
 
 FruitBST.prototype.findHeaviest = function () {
   var node = this;
+
   while (node.right !== undefined) {
     node = node.right;
   }
+
   console.log(node.value);
 };
 
 FruitBST.prototype.findLightest = function () {
   var node = this;
+
   while (node.left !== undefined) {
     node = node.left;
   }
+
   console.log(node.value);
 };
 
 var bst = FruitBST(apple);
 bst.insert(orange);
 bst.insert(mango);
-bst.insert(tomato);
-//console.log(bst);
-bst.iterate();
-//bst.findHeaviest();
+bst.insert(tomato); //console.log(bst);
+
+bst.iterate(); //bst.findHeaviest();
 //bst.findLightest();
 //bst.filterByWeight(150);
 //bst.filterbytype("mango fruit");
 //bst.filterByWeight(200);
 //console.log("*******");
-var newbst1 = bst.magnifyByType("tomato fruit" , 145);
+
+var newbst1 = bst.magnifyByType("tomato fruit", 145);
 console.log("*******");
 newbst1.iterate();
